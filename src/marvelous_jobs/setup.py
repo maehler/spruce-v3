1c8ef5e89623be74ed0b2338d4d623ceae9be7cc
Niklas Mähler <niklas.mahler@gmail.com>
2018-08-30 12:06:02 +0200

    Make it a proper python module

---
+++ src/marvelous_jobs/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='marvelous_jobs',
+    version='0.1.0',
+    packages=find_packages(),
+
+    entry_points={
+        'console_scripts': [
+            'marvelous_jobs = marvelous_jobs.__main__:main'
+        ]
+    },
+
+    author='Niklas Mähler',
+    author_email='niklas.mahler@gmail.com',
+    description='Job manager for the MARVEL assembler',
+    url='https://github.com/bschiffthaler/spruce-v3/tree/master/src/marvelous_jobs'
+)
