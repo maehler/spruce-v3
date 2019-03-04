3a8a330f91c8b23e5d2d239ba13960908b58f3b8
Niklas Mähler <niklas.mahler@nmbu.no>
2019-03-04 13:17:53 +0100

    Only update on an actual change

--- src/marvelous_jobs/marvelous_jobs/config.py
+++ src/marvelous_jobs/marvelous_jobs/config.py
@@ -66,13 +66,14 @@ class marvelous_config:
         self.save()
 
     def update(self, section, key, value, default=None):
-        """Update a config value
+        """Update a config value.
 
         If `value` is `None` and the key has not been set to
         a non-`None` value already, then the key is set to
-        the default value. If the key has been set before,
-        and `value` is not `None`, then the key will have the
-        value `value`. Otherwise the key will be set to `None`.
+        the default value. If `value` is not `None` the key
+        will get this value. If neither of these cases apply
+        it means the key already has a value and that `value`
+        is `None`, and nothing will changed.
 
         Parameters
         ----------
@@ -90,10 +91,10 @@ class marvelous_config:
         if value is None and self.get(section, key) is None:
             self.set(section, key, default)
         elif value is not None:
-            self.set(section, key, value)
-        else:
-            self.set(section, key, None)
+            if self.get(section, key) != value:
+                self.set(section, key, value)
 
     def save(self):
+        """Save the config to file."""
         with open(self.filename, 'w') as f:
             self.config.write(f)
