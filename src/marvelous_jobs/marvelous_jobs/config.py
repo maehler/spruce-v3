61b8559ce2206692b3b48cb677eda1f621ac5b46
Niklas Mähler <niklas.mahler@nmbu.no>
2019-03-04 13:04:53 +0100

    Add function for updating values

--- src/marvelous_jobs/marvelous_jobs/config.py
+++ src/marvelous_jobs/marvelous_jobs/config.py
@@ -46,8 +46,53 @@ class marvelous_config:
                 self.config.set(section, key, str(value))
 
     def set(self, section, key, value):
+        """Set a config value.
+
+        If `section` does not yet exist, it is created.
+
+        Parameters
+        ----------
+        section : str
+            Name of the section to set the key in.
+        key : str
+            Name of the key to set the value for.
+        value : object
+            The value to set for the key. This object is
+            saved as `str(value)`.
+        """
+        if not self.config.has_section(section):
+            self.config.add_section(section)
         self.config.set(section, key, str(value))
         self.save()
+
+    def update(self, section, key, value, default=None):
+        """Update a config value
+
+        If `value` is `None` and the key has not been set to
+        a non-`None` value already, then the key is set to
+        the default value. If the key has been set before,
+        and `value` is not `None`, then the key will have the
+        value `value`. Otherwise the key will be set to `None`.
+
+        Parameters
+        ----------
+        section : str
+            Name of the section to set the key in.
+        key : str
+            Name of the key to set the value for.
+        value : object
+            The value to set for the key. This object is
+            saved as `str(value)`.
+        default : object, optional
+            The value to set if `value` is `None`. This object
+            is saved as `str(value)`.
+        """
+        if value is None and self.get(section, key) is None:
+            self.set(section, key, default)
+        elif value is not None:
+            self.set(section, key, value)
+        else:
+            self.set(section, key, None)
 
     def save(self):
         with open(self.filename, 'w') as f:
