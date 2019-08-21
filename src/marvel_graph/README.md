68d09bbfc6e4267f0f15708f51b757795425e37b
Niklas Mähler <niklas.mahler@gmail.com>
2019-08-21 10:27:20 +0200

    Graph creation script

---
+++ src/marvel_graph/README.md
@@ -0,0 +1,11 @@
+# MARVEL overlap graph
+
+## `create_graph.sh`
+
+Create the overlap graph from the filtered alignments.
+
+## `tour_graph.sh`
+
+Tour the overlap graph components.
+
+## `contigs.sh`
---
+++ src/marvel_graph/create_graph.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+
+#SBATCH --account=snic2018-3-317
+#SBATCH --partition=core
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1:00:00
+#SBATCH --job-name=spruce_og
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=niklas.mahler@umu.se
+
+set -eu
+
+if [[ $# -ne 2 ]]; then
+    echo >&2 "usage: $0 <database> <input las>"
+    exit 1
+fi
+
+module load gnuparallel/20180822
+source /proj/uppstore2017145/V3/software/activate.sh
+
+database=$1
+input_las=$2
+
+if [[ ! -f ${input_las} ]]; then
+    echo "error: file or directory not found: ${input_las}" >&2
+fi
+
+trim_track=gap_trim
+n_edges_to_dead_ends=1
+output_dir=$(dirname ${input_las})/components
+
+if [[ ! -d ${output_dir} ]]; then
+    mkdir ${output_dir}
+fi
+
+OGbuild \
+    -s \
+    -t ${trim_track} \
+    -c ${n_edges_to_dead_ends} \
+    ${database} \
+    ${input_las} \
+    ${output_dir}/${database}
